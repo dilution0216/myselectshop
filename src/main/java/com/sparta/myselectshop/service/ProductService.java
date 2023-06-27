c13c631b1d82ccab9aa78cd408de7c503dd0f4ba
dilution0216 <hglad0216@naver.com>
2023-06-27 17:49:01 +0900

    폴더 생성 및 조회 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -47,7 +47,7 @@ public class ProductService {
 
 
     }
-
+    @Transactional(readOnly = true)
     public Page<ProductResponseDto> getProduct(User user, int page, int size, String sortBy, boolean isAsc) {
         Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, sortBy);
